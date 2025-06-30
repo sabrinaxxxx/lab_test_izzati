c9b2c8cb8e239730323c55853cd98bf1f588d1dc
sabrinaxxxx <nurulsabrina946@gmail.com>
2025-06-30 18:09:16 +0800

    Update application ID and Firebase configuration for lab_test_izzati

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_lab_sabrina"
+        applicationId = "com.example.lab_test_izzati"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
